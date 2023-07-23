109c1cf97e8751ee29af99d0c9a2cdc75f9914dc
smalaca <ab13.krakow@gmail.com>
2023-07-23 21:31:14 +0200

    DRAFT with two empty application to verify docker-compose.yml

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Online shop
+
+---
+
+### How to run application
+To run application you need to:
+1. Have installed Docker with Docker Compose.
+2. Run `docker-compose up --build` in main directory with `docker-compose.yml` file to build and run application.
---
+++ app-two/src/main/java/com/smalaca/apptwo/Controller.java
@@ -0,0 +1,14 @@
+package com.smalaca.apptwo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hi")
+public class Controller {
+    @GetMapping
+    public String world() {
+        return "Hello world form app TWO";
+    }
+}
