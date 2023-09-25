48768755498386e9d6e00784ab426f3685653ce1
smalaca <ab13.krakow@gmail.com>
2023-09-25 21:46:29 +0200

    ISSUE-10 - remove OfferId

--- purchase/src/main/java/com/smalaca/purchase/domain/offer/OfferRepository.java
+++ purchase/src/main/java/com/smalaca/purchase/domain/offer/OfferRepository.java
@@ -3,10 +3,12 @@ package com.smalaca.purchase.domain.offer;
 import com.smalaca.annotations.architectures.portadapter.SecondaryPort;
 import com.smalaca.annotations.ddd.Repository;
 
+import java.util.UUID;
+
 @Repository
 @SecondaryPort
 public interface OfferRepository {
     void save(Offer offer);
 
-    Offer findById(OfferId offerId);
+    Offer findById(UUID offerId);
 }
