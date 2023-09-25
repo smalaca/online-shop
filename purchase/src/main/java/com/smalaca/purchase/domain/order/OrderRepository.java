eec6feed2a25cd6654382982ae040f4b91ffd625
smalaca <ab13.krakow@gmail.com>
2023-09-25 21:47:10 +0200

    ISSUE-10 - remove OrderId

--- purchase/src/main/java/com/smalaca/purchase/domain/order/OrderRepository.java
+++ purchase/src/main/java/com/smalaca/purchase/domain/order/OrderRepository.java
@@ -3,10 +3,12 @@ package com.smalaca.purchase.domain.order;
 import com.smalaca.annotations.architectures.portadapter.SecondaryPort;
 import com.smalaca.annotations.ddd.Repository;
 
+import java.util.UUID;
+
 @SecondaryPort
 @Repository
 public interface OrderRepository {
     void save(Order order);
 
-    Order findById(OrderId orderId);
+    Order findById(UUID orderId);
 }
